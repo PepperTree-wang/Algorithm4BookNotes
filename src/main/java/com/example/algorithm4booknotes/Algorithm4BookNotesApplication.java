32b08459e24e961c05961b561a94e56211bec23e
peppertree <dragon_ch_z@163.com>
2022-11-28 22:41:16 +0800

    Initial commit

---
+++ src/main/java/com/example/algorithm4booknotes/Algorithm4BookNotesApplication.java
@@ -0,0 +1,13 @@
+package com.example.algorithm4booknotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Algorithm4BookNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Algorithm4BookNotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/algorithm4booknotes/Algorithm4BookNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.algorithm4booknotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Algorithm4BookNotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
