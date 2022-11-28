bd8419b835502ff4b72de5c284df483bd4d1c6c9
peppertree <dragon_ch_z@163.com>
2022-11-29 07:09:33 +0800

    # mod 添加算法包以及创建第一章节的说明

---
+++ src/main/java/com/example/algorithm4booknotes/chapter_1/AlgorithmBasic.java
@@ -0,0 +1,15 @@
+package com.example.algorithm4booknotes.chapter_1;
+
+public class AlgorithmBasic {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 使用函数绘制函数曲线
+     */
+    public void printImageline(){
+        int n = 100;
+//        StdDraw.set
+    }
+}
